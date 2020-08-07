873a1967a2b081bb2a40a4f6fe63847247c177ff
Din <diniskakov@gmail.com>
2020-08-07 18:08:13 +0200

    2nd problem solved

--- moduledatetime.py
+++ moduledatetime.py
@@ -1,4 +1,17 @@
 import datetime
 
+'''
 now = datetime.datetime.today().strftime("%B, %d, %Y, %H:%M PM")
 print(now)
+'''
+
+year = int(input('Type down your birthday year: '))
+month = int(input('Type down your birthday month: '))
+day = int(input('Type down your birthday day: '))
+
+today = datetime.datetime.today()
+birthday = datetime.datetime(year, month, day)
+days_past = today - birthday
+real_days_past = days_past.days
+
+print(str(real_days_past) + ' days past since your birth')
