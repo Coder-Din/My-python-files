6f660abdb0cf4fa04fd39cf40fd843be7f7f7b73
Din <diniskakov@gmail.com>
2020-08-07 18:30:22 +0200

    Last problem solved

--- moduledatetime.py
+++ moduledatetime.py
@@ -5,6 +5,7 @@ now = datetime.datetime.today().strftime("%B, %d, %Y, %H:%M PM")
 print(now)
 '''
 
+'''
 year = int(input('Type down your birthday year: '))
 month = int(input('Type down your birthday month: '))
 day = int(input('Type down your birthday day: '))
@@ -15,3 +16,13 @@ days_past = today - birthday
 real_days_past = days_past.days
 
 print(str(real_days_past) + ' days past since your birth')
+'''
+
+year = int(input('Type down a year: '))
+for elem in range(12):
+    elem += 1
+    date = datetime.datetime(year, elem, 13)
+    if date.weekday() == 4:
+        date = date.strftime("%Y-%d-%w")
+        print('Friday 13-th found: ' + str(date))
+
